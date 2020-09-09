68b9bc1aeaa364eeaa32616c9418710c520f4970
a-abidi <aliabidi@icloud.com>
2020-09-09 15:19:02 +0100

    Move WebView to its own file.

---
+++ HackerFeed/Views/WebView.swift
@@ -0,0 +1,28 @@
+//
+//  WebView.swift
+//  HackerFeed
+//
+//  Created by Ali on 09/09/2020.
+//  Copyright © 2020 Ali Abidi. All rights reserved.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WebView.UIViewType {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
