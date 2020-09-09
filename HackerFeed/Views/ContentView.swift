b7f088b8ac0ca5242df5f3642eb352d727c7c840
a-abidi <aliabidi@icloud.com>
2020-09-09 15:08:56 +0100

    Create detail view for more info on links.

--- HackerFeed/Views/ContentView.swift
+++ HackerFeed/Views/ContentView.swift
@@ -15,7 +15,11 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            List(networkManager.posts) { post in Text(post.title) }
+            List(networkManager.posts) { post in HStack {
+                Text(String(post.points))
+                Text(post.title)
+                }
+            }
                 .navigationBarTitle("HackerFeed")
         }
         .onAppear {
---
+++ HackerFeed/Views/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  HackerFeed
+//
+//  Created by Ali on 09/09/2020.
+//  Copyright © 2020 Ali Abidi. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
